fe5a9c50f594b47f9d080ce321a49ba6464de514
Adrian Lai <adrian.lai@jetstack.io>
2026-02-27 22:23:51 +0000

    WIP: PushSecret implementation for conjur
    very very work in progress.

--- providers/v1/conjur/client.go
+++ providers/v1/conjur/client.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/cyberark/conjur-api-go/conjurapi"
 	"github.com/cyberark/conjur-api-go/conjurapi/authn"
@@ -95,9 +96,40 @@ func (c *Client) GetConjurClient(ctx context.Context) (SecretsClient, error) {
 	return nil, errors.New("no authentication method provided")
 }
 
-// PushSecret will write a single secret into the provider.
-func (c *Client) PushSecret(_ context.Context, _ *corev1.Secret, _ esv1.PushSecretData) error {
-	return errors.New("pushing secrets is not implemented for the Conjur provider")
+func (c *Client) PushSecret(ctx context.Context, secret *corev1.Secret, ref esv1.PushSecretData) error {
+	conjurClient, getConjurClientError := c.GetConjurClient(ctx)
+	if getConjurClientError != nil {
+		return getConjurClientError
+	}
+
+	val, ok := secret.Data[ref.GetSecretKey()]
+	if !ok {
+		return errors.New("key not found")
+	}
+
+	//title := ref.GetRemoteKey()
+	policy := `
+- !policy
+  id: fromk8s
+  body:
+  - !variable
+    id: k8ssecret
+
+- !permit
+  resource: !variable fromk8s/k8ssecret
+  role: !host /data/tlspc
+  privileges: [ read, execute, update ]
+`
+
+	_, err := conjurClient.LoadPolicy(conjurapi.PolicyModePost, "data/tlspc", strings.NewReader(policy))
+	if err != nil {
+		return err
+	}
+	err = conjurClient.AddSecret(fmt.Sprintf("%s/%s/k8ssecret", "data/tlspc", "fromk8s"), string(val))
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // DeleteSecret removes a secret from the provider.
