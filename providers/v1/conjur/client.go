54790cb7babfbf74b438817187b72d8d95bf29ae
Adrian Lai <adrian.lai@jetstack.io>
2026-03-02 10:37:00 +0000

    WIP: Respect provided secret name

--- providers/v1/conjur/client.go
+++ providers/v1/conjur/client.go
@@ -17,10 +17,12 @@ limitations under the License.
 package conjur
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
 	"strings"
+	"text/template"
 
 	"github.com/cyberark/conjur-api-go/conjurapi"
 	"github.com/cyberark/conjur-api-go/conjurapi/authn"
@@ -96,6 +98,36 @@ func (c *Client) GetConjurClient(ctx context.Context) (SecretsClient, error) {
 	return nil, errors.New("no authentication method provided")
 }
 
+const policyTemplate = `
+- !policy
+  id: {{ .Name }}
+  body:
+  - !variable
+    id: {{ .Variable }}
+
+- !permit
+  resource: !variable {{ .Name }}/{{ .Variable }}
+  role: !host {{ .Owner }}
+  privileges: [ read, execute, update ]
+`
+
+func conjurPolicy(name, variable, user string) string {
+	type policy struct {
+		Name     string
+		Owner    string
+		Variable string
+	}
+	p := policy{
+		Name:     name,
+		Owner:    user,
+		Variable: variable,
+	}
+	t := template.Must(template.New("policy").Parse(policyTemplate))
+	buf := &bytes.Buffer{}
+	t.Execute(buf, p)
+	return buf.String()
+}
+
 // PushSecret will write a single secret into the provider.
 func (c *Client) PushSecret(ctx context.Context, secret *corev1.Secret, ref esv1.PushSecretData) error {
 	conjurClient, getConjurClientError := c.GetConjurClient(ctx)
@@ -108,25 +140,26 @@ func (c *Client) PushSecret(ctx context.Context, secret *corev1.Secret, ref esv1
 		return errors.New("key not found")
 	}
 
-	//title := ref.GetRemoteKey()
-	policy := `
-- !policy
-  id: fromk8s
-  body:
-  - !variable
-    id: k8ssecret
+	fqSecretName := ref.GetRemoteKey()
+	// if property is empty, we should create multiple variables for each key of the secret
+	secretKey := ref.GetProperty()
+	i := strings.LastIndex(fqSecretName, "/")
+	if i == -1 {
+		return errors.New("Expected RemoteKey to contain a '/'")
+	}
+	if secretKey == "" {
+		return errors.New("property required")
+	}
+	parentPolicy := fqSecretName[0:i]
+	policyName := fqSecretName[i+1:]
+	// TODO: Determine user
+	policy := conjurPolicy(policyName, secretKey, "/data/tlspc")
 
-- !permit
-  resource: !variable fromk8s/k8ssecret
-  role: !host /data/tlspc
-  privileges: [ read, execute, update ]
-`
-
-	_, err := conjurClient.LoadPolicy(conjurapi.PolicyModePost, "data/tlspc", strings.NewReader(policy))
+	_, err := conjurClient.LoadPolicy(conjurapi.PolicyModePost, parentPolicy, strings.NewReader(policy))
 	if err != nil {
 		return err
 	}
-	err = conjurClient.AddSecret(fmt.Sprintf("%s/%s/k8ssecret", "data/tlspc", "fromk8s"), string(val))
+	err = conjurClient.AddSecret(fmt.Sprintf("%s/%s", fqSecretName, secretKey), string(val))
 	if err != nil {
 		return err
 	}
